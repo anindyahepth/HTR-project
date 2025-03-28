5e39c7fc9a673b75186f9255e51e841df77d1b3a
anindyahepth <129802283+anindyahepth@users.noreply.github.com>
2025-03-28 01:32:12 -0400

    Update option.py

--- utils/option.py
+++ utils/option.py
@@ -7,6 +7,7 @@ def get_args_parser():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('--out-dir', type=str, default='/content/HTR-project/output', help='output directory')
+    parser.add_argument('--pth_path', type=str, default='/content/HTR-project/best_CER.pth')
     parser.add_argument('--model-type', type=str, default='vitmae', help='choice of model')
     parser.add_argument('--train-bs', default=128, type=int, help='train batch size')
     parser.add_argument('--val-bs', default=8, type=int, help='validation batch size')
