f37a68209a0302fb15553c4b0fedc8bccffbb4a6
anindyahepth <129802283+anindyahepth@users.noreply.github.com>
2025-06-18 02:27:14 -0400

    Update resnet18.py

--- model/resnet18.py
+++ model/resnet18.py
@@ -14,21 +14,22 @@ class ResNet18_feat_ex(nn.Module):
           self.backbone = timm.create_model('resnet18.a1_in1k', pretrained=False, features_only=True)
 
         #Changing feature_dim to desired value
-        self.feature_modifier_conv = nn.Sequential(
-            nn.Conv2d(256, embed_dim, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(embed_dim),
-            nn.ReLU(inplace=True)
-        )
+        # self.feature_modifier_conv = nn.Sequential(
+        #     nn.Conv2d(256, embed_dim, kernel_size=1, stride=1, padding=0),
+        #     nn.BatchNorm2d(embed_dim),
+        #     nn.ReLU(inplace=True)
+        #)
 
         
 
     def forward(self, x):
         feature_maps = self.backbone(x)
-        features_from_layer3 = feature_maps[3] # Shape: (Batch, 256, H, W)
-        modified_features = self.feature_modifier_conv(features_from_layer3) # Shape: (Batch, new_feature_dim, H, W)
+        features_from_layer_3 = feature_maps[3] # Shape: (Batch, 256, H, W)
+        #modified_features = self.feature_modifier_conv(features_from_layer3) # Shape: (Batch, new_feature_dim, H, W)
 
         
-        output = modified_features
+        #output = modified_features
+        output = features_from_layer_3
 
         return output
 
