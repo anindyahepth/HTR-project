adb8e2464b5b59f354822e83ca0e5ed4ae9f04e1
anindyahepth <129802283+anindyahepth@users.noreply.github.com>
2025-06-20 00:09:10 -0400

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -40,22 +40,7 @@ def rand_uniform(low, high):
     return float(Uniform(low, high).sample())
 
 
-def get_logger(out_dir):
-    logger = logging.getLogger('Exp')
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
-
-    file_path = os.path.join(out_dir, "run.log")
-    file_hdlr = logging.FileHandler(file_path)
-    file_hdlr.setFormatter(formatter)
-
-    strm_hdlr = logging.StreamHandler(sys.stdout)
-    strm_hdlr.setFormatter(formatter)
-
-    logger.addHandler(file_hdlr)
-    logger.addHandler(strm_hdlr)
-    return logger
-
+#Learning rate scheduler
 
 def update_lr_cos(nb_iter, warm_up_iter, total_iter, max_lr, optimizer, min_lr=1e-7):
 
@@ -69,6 +54,7 @@ def update_lr_cos(nb_iter, warm_up_iter, total_iter, max_lr, optimizer, min_lr=1
 
     return optimizer, current_lr
 
+#CTC converter class
 
 class CTCLabelConverter(object):
     def __init__(self, character):
@@ -110,96 +96,3 @@ class CTCLabelConverter(object):
             texts.append(text)
             index += l
         return texts
-
-
-class Averager(object):
-    def __init__(self):
-        self.reset()
-
-    def add(self, v):
-        count = v.data.numel()
-        v = v.data.sum()
-        self.n_count += count
-        self.sum += v
-
-    def reset(self):
-        self.n_count = 0
-        self.sum = 0
-
-    def val(self):
-        res = 0
-        if self.n_count != 0:
-            res = self.sum / float(self.n_count)
-        return res
-
-
-class Metric(object):
-    def __init__(self, name=''):
-        self.name = name
-        self.sum = torch.tensor(0.).double()
-        self.n = torch.tensor(0.)
-
-    def update(self, val):
-        rt = val.clone()
-        dist.all_reduce(rt, op=dist.ReduceOp.SUM)
-        rt /= dist.get_world_size()
-        self.sum += rt.detach().cpu().double()
-        self.n += 1
-
-    @property
-    def avg(self):
-        return self.sum / self.n.double()
-
-
-class ModelEma:
-    def __init__(self, model, decay=0.9999, device='', resume=''):
-        self.ema = deepcopy(model)
-        self.ema.eval()
-        self.decay = decay
-        self.device = device
-        if device:
-            self.ema.to(device=device)
-        self.ema_has_module = hasattr(self.ema, 'module')
-        if resume:
-            self._load_checkpoint(resume)
-        for p in self.ema.parameters():
-            p.requires_grad_(False)
-
-    def _load_checkpoint(self, checkpoint_path, mapl=None):
-        checkpoint = torch.load(checkpoint_path,map_location=mapl)
-        assert isinstance(checkpoint, dict)
-        if 'state_dict_ema' in checkpoint:
-            new_state_dict = OrderedDict()
-            for k, v in checkpoint['state_dict_ema'].items():
-                if self.ema_has_module:
-                    name = 'module.' + k if not k.startswith('module') else k
-                else:
-                    name = k
-                new_state_dict[name] = v
-            self.ema.load_state_dict(new_state_dict)
-            print("=> Loaded state_dict_ema")
-        else:
-            print("=> Failed to find state_dict_ema, starting from loaded model weights")
-
-    def update(self, model, num_updates=-1):
-        needs_module = hasattr(model, 'module') and not self.ema_has_module
-        if num_updates >= 0:
-            _cdecay = min(self.decay, (1 + num_updates) / (10 + num_updates))
-        else:
-            _cdecay = self.decay
-
-        with torch.no_grad():
-            msd = model.state_dict()
-            for k, ema_v in self.ema.state_dict().items():
-                if needs_module:
-                    k = 'module.' + k
-                model_v = msd[k].detach()
-                if self.device:
-                    model_v = model_v.to(device=self.device)
-                ema_v.copy_(ema_v * _cdecay + (1. - _cdecay) * model_v)
-
-
-def format_string_for_wer(str):
-    str = re.sub('([\[\]{}/\\()\"\'&+*=<>?.;:,!\-—_€#%°])', r' \1 ', str)
-    str = re.sub('([ \n])+', " ", str).strip()
-    return str
